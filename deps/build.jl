1ce54cbcec22209de07dd477eab6a06dc043cf2e
Alexander Barth <barth.alexander@gmail.com>
2018-06-28 14:07:56 +0200

    native package manager on Linux and Homebrew on OSX

--- deps/build.jl
+++ deps/build.jl
@@ -27,9 +27,13 @@ end
 @BinDeps.setup
 libnetcdf = library_dependency("libnetcdf", aliases = ["libnetcdf4","libnetcdf-7","netcdf"], validate = validate_netcdf_version)
 
-#Conda.add_channel("conda-forge")
 provides(Conda.Manager, "libnetcdf", libnetcdf)
 provides(AptGet, "libnetcdf-dev", libnetcdf, os = :Linux)
 provides(Yum, "netcdf-devel", libnetcdf, os = :Linux)
+
+@osx_only begin
+    using Homebrew
+    provides( Homebrew.HB, "netcdf", libnetcdf, os = :Darwin )
+end
 
 @BinDeps.install Dict(:libnetcdf => :libnetcdf)
